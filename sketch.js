027a0cbe8229e17701eb3b89090e009f2d7bf133
rohit-Ironman <72317546+rohit-Ironman@users.noreply.github.com>
2020-12-17 13:35:11 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 var dog, dogIMG, dogHappyIMG, database;
 var foodS, foodStock;
-var foodCount = 20; 
+
 function preload()
 {
-  dogIMG = loadImage("images/dogImg.png");
+  dogIMG = loadImage("images/dogImg.png")
+  dogHappyIMG = loadImage("images/dogImg1.png")
 
 }
 
 function setup() {
   createCanvas(500, 500);
-
+  database = firebase.database();
+  foodStock = database.ref("food");
+  foodStock.on("value", readStock);
   dog = createSprite(250, 250, 10,10); 
   dog.scale = 0.3;
   dog.addImage(dogIMG);
@@ -18,22 +21,31 @@ function setup() {
 function draw() {  
 
   if(keyWentDown(UP_ARROW)){
-    foodCount = foodCount - 1;
+   writeStock(foodS);
+   dog.addImage(dogHappyIMG);
     
   }
   background("green");
   fill("red"); 
   textSize(15);
   text("PRESS THE UPP ARR0W KEY TO FEED THE FRICKIN' DOG",50, 20);
-  if(foodCount <= 0){
-    foodCount = 0;
-  }
-  text("Food Remaining: " + foodCount, 200, 50);
+
+  text("Food Remaining: " + foodS, 200, 50);
  
  
  
   drawSprites();
   
 }
-
+function readStock(data){
+  foodS = data.val();
+}
+function writeStock(foodCount){
+  
+  if(foodCount<=0){
+    foodCount = 0
+  }
+  else{foodCount = foodCount - 1}
+  database.ref("/").update({food: foodCount});
+}
 
